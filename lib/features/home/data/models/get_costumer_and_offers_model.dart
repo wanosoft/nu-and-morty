35699741d7913d72f95c944a5be254ec75a3fb01
Anonymous Developer <anonymous@example.com>
2021-09-12 21:53:23 -0500

    Add get costumer and offers model

---
+++ lib/features/home/data/models/get_costumer_and_offers_model.dart
@@ -0,0 +1,71 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'get_costumer_and_offers_model.g.dart';
+
+@JsonSerializable()
+class GetCostumerAndOffersModel extends Equatable {
+  final String id;
+  final String name;
+  final int balance;
+  final List<OfferModel> offers;
+
+  const GetCostumerAndOffersModel({
+    required this.id,
+    required this.name,
+    required this.balance,
+    required this.offers,
+  });
+
+  factory GetCostumerAndOffersModel.fromJson(Map<String, dynamic> json) =>
+      _$GetCostumerAndOffersModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetCostumerAndOffersModelToJson(this);
+
+  @override
+  List<Object> get props => [id, name, balance, offers];
+}
+
+@JsonSerializable()
+class OfferModel extends Equatable {
+  final String id;
+  final int price;
+  final ProductModel product;
+
+  const OfferModel({
+    required this.id,
+    required this.price,
+    required this.product,
+  });
+
+  factory OfferModel.fromJson(Map<String, dynamic> json) =>
+      _$OfferModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OfferModelToJson(this);
+
+  @override
+  List<Object> get props => [id, price, product];
+}
+
+@JsonSerializable()
+class ProductModel extends Equatable {
+  final String id;
+  final String name;
+  final String description;
+  final String image;
+
+  const ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.image,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) =>
+      _$ProductModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProductModelToJson(this);
+
+  @override
+  List<Object> get props => [id, name, description, image];
+}
