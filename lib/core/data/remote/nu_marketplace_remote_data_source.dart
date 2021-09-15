c721e2e3d76e4aaf9346def367154303a66b9c88
Anonymous Developer <anonymous@example.com>
2021-09-15 01:16:39 -0500

    Add nu marketplace remote data source

---
+++ lib/core/data/remote/nu_marketplace_remote_data_source.dart
@@ -0,0 +1,28 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:injectable/injectable.dart';
+import 'package:nu_and_morty/core/data/constants.dart';
+import 'package:nu_and_morty/features/home/data/models/get_costumer_and_offers_model.dart';
+
+abstract class NuMarketplaceRemoteDataSource {
+  Future<GetCostumerAndOffersModel> getCostumerAndOffersModel();
+}
+
+@LazySingleton(as: NuMarketplaceRemoteDataSource)
+class NuMarketplaceRemoteDataSoucerImpl
+    implements NuMarketplaceRemoteDataSource {
+  const NuMarketplaceRemoteDataSoucerImpl(this._client);
+
+  final GraphQLClient _client;
+
+  @override
+  Future<GetCostumerAndOffersModel> getCostumerAndOffersModel() async {
+    try {
+      final result = await _client.query(QueryOptions(
+        document: gql(GraphQLQueries.getCostumerAndOffersQuery),
+      ));
+      return GetCostumerAndOffersModel.fromJson(result.data?['viewer']);
+    } on Exception {
+      throw const ServerException();
+    }
+  }
+}
--- lib/core/injection/injector.config.dart
+++ lib/core/injection/injector.config.dart
@@ -8,7 +8,8 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:graphql_flutter/graphql_flutter.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'register_module.dart' as _i4; // ignore_for_file: unnecessary_lambdas
+import '../data/remote/nu_marketplace_remote_data_source.dart' as _i4;
+import 'register_module.dart' as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -17,7 +18,9 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   final registerModule = _$RegisterModule();
   gh.lazySingleton<_i3.GraphQLClient>(() => registerModule.gqlClient);
+  gh.lazySingleton<_i4.NuMarketplaceRemoteDataSource>(
+      () => _i4.NuMarketplaceRemoteDataSoucerImpl(get<_i3.GraphQLClient>()));
   return get;
 }
 
-class _$RegisterModule extends _i4.RegisterModule {}
+class _$RegisterModule extends _i5.RegisterModule {}
