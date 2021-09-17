cf268267a4cb5960dae324b14b287fda9d8574d6
Anonymous Developer <anonymous@example.com>
2021-09-17 10:22:50 -0500

    Add purchase offer repository

--- lib/core/domain/failure.dart
+++ lib/core/domain/failure.dart
@@ -1,17 +1,24 @@
+import 'package:equatable/equatable.dart';
 import 'package:nu_and_morty/core/presentation/internationalization.dart';
 
-abstract class Failure {
+abstract class Failure extends Equatable {
   String get errorMessage;
 }
 
 class ServerFailure extends Failure {
   @override
   String get errorMessage => Internationalization.serverErrorMessage;
+
+  @override
+  List<Object> get props => [errorMessage];
 }
 
 class UnknownFailure extends Failure {
   @override
   String get errorMessage => Internationalization.unknownErrorMessage;
+
+  @override
+  List<Object> get props => [errorMessage];
 }
 
 class NuMarketplaceFailure extends Failure {
@@ -21,4 +28,7 @@ class NuMarketplaceFailure extends Failure {
 
   @override
   String get errorMessage => _message;
+
+  @override
+  List<Object> get props => [_message];
 }
--- lib/core/domain/repositories/nu_marketplace_repository.dart
+++ lib/core/domain/repositories/nu_marketplace_repository.dart
@@ -1,6 +1,8 @@
 import 'package:nu_and_morty/core/domain/result.dart';
 import 'package:nu_and_morty/features/home/domain/entities/costumer_offers_entity.dart';
+import 'package:nu_and_morty/features/purchase/domain/entities/purchase_offer_entity.dart';
 
 abstract class NuMarketplaceRepository {
   Future<Result<CostumerOffersEntity>> getCostumerOffers();
+  Future<Result<PurchaseOfferEntity>> purchaseOffer(String id);
 }
